a0d7848da10594199e9d381459019a8f0f3bdbbe
chbsila <137139630+chbsila@users.noreply.github.com>
2026-01-29 14:45:31 -0500

    Update simulation.R

--- simulation.R
+++ simulation.R
@@ -15,7 +15,7 @@ r <- sim_ranking(n_good = 200, n_bad = 100, n_items = 50)
 
 table   <- intersection_model_B(200, 100)
 round  <- sim_round(r, table)$results
+# To truly simulate, I need to average out multiple trials!!
 taus <- sapply(1:200, function(i) cor(round[[i]]$posterior_ranking, r$good$true_rank, method="kendall"))
 plot(taus, pch=19, col="blue", main="Kendall's Tau", xlab="Good Process ID", ylab="Tau", ylim=c(0,1))
 abline(h=mean(taus), col="red", lty=2)
-
